6581d08eb28ecd172f444367f739f10b3a27e9f8
Nexus <egor2001grashin@gmail.com>
2024-07-04 02:49:11 +0300

    Hid the news block temporarily due to its lack of necessity

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,12 @@
 import GamesSection from "../../components/blocks-content/games/GamesSection";
-import NewsContainer from "../../components/blocks-content/news/NewsSection";
+/* import NewsContainer from "../../components/blocks-content/news/NewsSection"; */
 import PageLayout from "../../components/pageLayout/PageLayout"
 
 
 const Home = () => {
   return (
     <PageLayout>
-      <NewsContainer />
+      {/* <NewsContainer /> */}
       <GamesSection />
     </PageLayout>
   )
